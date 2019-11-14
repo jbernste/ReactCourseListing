5092f755f95f63672743b1592833ff4f33365060
j5shuatree <ankbitinfo@gmail.com>
2019-11-13 22:01:12 -0800

    initial Commit

---
+++ src/components/CoursesList.js
@@ -0,0 +1,9 @@
+import {Component} from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import * as contentful from 'contentful'
+import Course from '../components/Course'
+
+const SPACE_ID = ''
+cont ACCESS_TOKEN = ''
+
