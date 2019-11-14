facd328ca8a5a0c3962aec2a83134406a75e9d3f
j5shuatree <ankbitinfo@gmail.com>
2019-11-13 22:13:34 -0800

    added methods to courses
    added methods to courseList with Validation on searching

--- src/components/CoursesList.js
+++ src/components/CoursesList.js
@@ -4,6 +4,35 @@ import TextField from '@material-ui/core/TextField'
 import * as contentful from 'contentful'
 import Course from '../components/Course'
 
-const SPACE_ID = ''
-cont ACCESS_TOKEN = ''
+const SPACE_ID = 'temp'
+const ACCESS_TOKEN = 'temp'
+
+const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN
+})
+
+class CourseList extends Component {
+    state={
+        courses:[],
+        searchString: ''
+    }
+    cosntructor(){
+        super()
+        this.getCourses()
+    }
+    getCourses = () => {
+        client.getEntries({
+            content_type: 'course',
+            query: this.state.searchString
+        })
+        .then((response)=>{
+            this.setState({courses: response.items})
+        })
+        .catch((error) => {
+            console.log("Error occured while catching data")
+            console.log(error)
+        })
+    }
+}
 
