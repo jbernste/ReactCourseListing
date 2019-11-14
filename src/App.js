f26fc92e26e08a6fd8e55df3eb4cd0249051e585
j5shuatree <ankbitinfo@gmail.com>
2019-11-13 23:03:45 -0800

    added Course Component
    Added components Course Material UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import NavBar from './components/NavBar'
+import CoureList from './components/CourseList'
+
 import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-    <NavBar /><p></p>
-      <h1><text>Hello World!</text></h1>
-
-    </div>
-  );
+  
+    return (
+      <div>
+        <NavBar /> 
+       
+      
+        <p></p>
+        <h1><text>Hello World!</text></h1>
+      </div>
+    );
 }
 
 export default App;
